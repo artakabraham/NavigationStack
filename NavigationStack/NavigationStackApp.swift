8b503ee8c02960ed281eb752fcf760cf794d83b3
Artak <abrahamyan@outlook.com>
2023-06-26 17:11:42 +0400

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Artak Abrahamyan on 26.06.23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
